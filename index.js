1a86cb39f713a83c7785048f93b52cf05dfbadc1
lawrencek <spiceeast509@gmail.com>
2025-04-07 23:41:13 +0300

    Initial commit for Tipsy Tales

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ document.getElementById("ageform").addEventListener("submit", function(event) {
     let age = parseInt(document.getElementById("age").value);
 
     if (!age || age < 0) {
-        alert("⚠ Please enter a valid age.");
+      alert("⚠️ Please enter a valid age.");
     } else if (age >= 18) {
-        alert("✅ Access Granted! Welcome.");
+      alert("✅ Access Granted! Welcome.");
+      window.location.href = "molly.html"; // Redirect to main page
     } else {
-        alert("❌ Sorry, you must be at least 18 years old.");
+      alert("❌ Sorry, you must be at least 18 years old.");
     }
-});
+  });
